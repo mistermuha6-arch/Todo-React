4f7ac7fbb6315c8589713a0ba1d67072feb20fe6
mistermuha6-arch <mistermuha6@gmail.com>
2026-04-09 22:06:49 +0300

    added localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Heder from './Componets/Heder'
 import ListProblem from './Componets/ListProblem'
-
+import { useState,useEffect } from 'react'
 function App() {
- 
-
+ const [todos,setTodos] = useState(() => {
+  const saved = localStorage.getItem("todos")
+  return saved ? JSON.parse(saved) : []
+ })
+ useEffect(() => {
+  localStorage.setItem('todos',JSON.stringify(todos))
+ },[todos])
   return (
     <>
-     
      <Heder></Heder> 
      <ListProblem></ListProblem>
     </>
